5f4dd06f15208bfb384c2bc686446f07a133b0b2
april <apriltopolewski@gmail.com>
2020-12-14 20:20:25 -0500

    added notes and changed a few things

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-const foodEntries =document.querySelector("FoodTransaction-input");
-const billEntries =document.querySelector("billsTransaction-input");
-const entertainmentEntries =document.querySelector("entertainmentTransactionsInput");
-const clothingEntries =document.querySelector("clothingTransactionInput");
-const ctx = document.getElementById("PieChart").getContext("2d");
-let PieChart = new CharacterData(ctx,{
+const foodEntries =document.querySelector('.FoodTransaction-input');//THINK WE NEED TIFFANY'S INPUT HERE
+const billEntries =document.querySelector('.billsTransaction-input');
+const entertainmentEntries =document.querySelector('.entertainmentTransactionsInput');
+const clothingEntries =document.querySelector('.clothingTransactionInput');
+const ctx = document.getElementById('PieChart').getContext('2d');//CREATES 2D CANVAS
+let PieChart = new Chart(ctx,{
     type: 'pie',
     data:{
         labels: ['Food Purchases','Bill Purchases','Entertainment Purchases', 'Clothing Purchases'],
         datasets: [{
             label:'BudgetAppPieChart',
             data: [0,0,0,0],
-            backgroundColor:[#ff766b,blue,orange,yellow],
+            backgroundColor:['#ff766b','#dd3b79','#2adece','#eeeee'],
             borderWidth: 1
         }
     ]
@@ -18,11 +18,11 @@ let PieChart = new CharacterData(ctx,{
 });
 const updateChartValue=(input,dataOrder)=>{
     input.addEventListner('change', e =>{
-        PieChart.data.datasets[0].data[dataOrder]=e.target.value;
+        PieChart.data.datasets[0].data[dataOrder]= e.target.value;//maybe needs to be mychart.data.datasets
         PieChart.update();
 }); 
 };
 updateChartValue(FoodTransaction-input,0);
 updateChartValue(billsTransaction-input,1);
-updateChartValue(entertinmentTransactionInput,2);
+updateChartValue(entertainmentTransactionInput,2);
 updateChartValue(clothingTransactionInput,3);
