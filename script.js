d91459f2f051cfc1b06d795c62ce71ff9c153c7a
april <apriltopolewski@gmail.com>
2020-12-14 22:03:19 -0500

    changed a bunch- sorry!

--- script.js
+++ script.js
@@ -3,26 +3,32 @@ const billEntries =document.querySelector('.billsTransaction-input');
 const entertainmentEntries =document.querySelector('.entertainmentTransactionsInput');
 const clothingEntries =document.querySelector('.clothingTransactionInput');
 const ctx = document.getElementById('PieChart').getContext('2d');//CREATES 2D CANVAS
-let PieChart = new Chart(ctx,{
+let pieChart = new Chart(ctx,{
     type: 'pie',
     data:{
+
         labels: ['Food Purchases','Bill Purchases','Entertainment Purchases', 'Clothing Purchases'],
-        datasets: [{
-            label:'BudgetAppPieChart',
-            data: [0,0,0,0],
-            backgroundColor:['#ff766b','#dd3b79','#2adece','#eeeee'],
-            borderWidth: 1
-        }
-    ]
+        datasets: [
+            {
+
+                label:'BudgetAppPieChart',
+                data: [0,0,0,0],
+                backgroundColor:['#ff766b','#dd3b79','#2adece','#eeeeee'],
+                borderWidth: 1,
+            }
+        ]
+
     }
 });
+
+
 const updateChartValue=(input,dataOrder)=>{
-    input.addEventListner('change', e =>{
-        PieChart.data.datasets[0].data[dataOrder]= e.target.value;//maybe needs to be mychart.data.datasets
-        PieChart.update();
-}); 
+    input.addEventListener("change", e =>{
+      PieChart.data.datasets[0].data[dataOrder] = e.target.value;
+      PieChart.update();
+    }); 
 };
-updateChartValue(FoodTransaction-input,0);
-updateChartValue(billsTransaction-input,1);
-updateChartValue(entertainmentTransactionInput,2);
-updateChartValue(clothingTransactionInput,3);
+updateChartValue(foodEntries,0);
+updateChartValue(billEntries,1);
+updateChartValue(entertainmentEntries,2);
+updateChartValue(clothingEntries,3);
